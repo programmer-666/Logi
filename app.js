ae562493e9385797527eed9123ea91ac27c7d7df
programmer-666 <34501436+programmer-666@users.noreply.github.com>
2024-04-02 22:31:03 +0300

    update

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-import express from "express";
 import path from "path";
 import morgan from "morgan";
+import express from "express";
+import bodyParser from "body-parser";
 
 
 const app = express();
 
 app.use(morgan("tiny"));
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/login", (req, res) => {
+    if (req.body["username"] === "root" || req.body["password"] === "123") {
+        res.sendFile(path.resolve("./public/success.html"));
+    } else {
+        res.sendFile(path.resolve("./public/failed.html"));
+    }
+});
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve("./public/index.html"));
